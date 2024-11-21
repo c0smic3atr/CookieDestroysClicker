45cf89e3b73b46411671e379a6ffdaf26ee56d94
c0smic3atr <marsdatrandomdemon@gmail.com>
2024-11-21 09:43:29 -0800

    bullets hit destroys asteroids, asteroids destroys player

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class AsteroidController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        if(collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
