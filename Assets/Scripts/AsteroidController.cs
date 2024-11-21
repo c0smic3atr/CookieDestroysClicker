559a9fc65147f21c136d940bf6498c484bdb04e6
c0smic3atr <marsdatrandomdemon@gmail.com>
2024-11-21 10:19:39 -0800

    Started Score and Asteroid breaks into smaller asteroids

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class AsteroidController : MonoBehaviour
 {
+    public int pointValue;
+    private GameManager gm;
+    
+    public GameObject smallerPointer;
+
+    private void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Bullet"))
         {
+            gm.AddScore(pointValue);
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
@@ -15,6 +26,19 @@ public class AsteroidController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
+        }
+    }
+
+    private void SpawnSmaller(int numberToSpawn)
+    {
+        if(smallerPointer != null)
+        {
+            for (int = i = 0; i < numberToSpawn; i++)
+            {
+                Instantiate(smallerPointer, transform.position, transform.rotation);
+
+            }
+            
         }
     }
 }
